930a3cbab984cf7ac93e0f716de02f500f89d342
Romadhan Edy Prasetyo <romadhanedy@gmail.com>
2021-12-08 12:52:38 +0700

    Clean(code): Remove useless code

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import os
 import sys
-import datetime
-from termcolor import colored, cprint # Impor termcolor modules
-from dotenv import load_dotenv as LoadDotEnv # Impor env modules
-LoadDotEnv() # Load env data
 import app.home as home # Impor home file
 
-# Deklarasi variabel dari env
-APP_BRAND_NAME = os.getenv('APP_BRAND_NAME')
-APP_BRAND_ADDRESS = os.getenv('APP_BRAND_ADDRESS')
-APP_BRAND_EMAIL = os.getenv('APP_BRAND_EMAIL')
-APP_BRAND_PHONE_NUMBER = os.getenv('APP_BRAND_PHONE_NUMBER')
-
 # Load main menu
-
 os.system('cls')
 home.MenuMain()
